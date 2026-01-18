59b9493158ea2c716b820e7c4cb7f9166cf148a7
jwoodsiii <woods3.jessie@gmail.com>
2026-01-17 22:17:45 -0800

    Adding initial repl loop and clean user input

--- repl.go
+++ repl.go
@@ -1,6 +1,27 @@
 package main
 
-import "strings"
+import (
+	"os"
+	"fmt"
+	"strings"
+	"bufio"
+)
+
+func startRepl() {
+	// create scanner for user input
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	for scanner.Scan() {
+		input := scanner.Text()
+		if len(input) == 0 {
+			continue
+		}
+		cleaned := cleanInput(input)
+		first := cleaned[0]
+		fmt.Printf("Your command was: %s\n", first)
+		fmt.Print("Pokedex > ")
+	}
+}
 
 func cleanInput(text string) []string {
 	// split the user's input into "words" based on whitespace.also
